7543ac6e2717ad1de8e8a19ccae03a04d753d010
Nicoll Oliver <nicolldon96@gmail.com>
2021-02-03 21:16:36 -0500

    commit tree 6fbe7a8d91b2eed47e753ace078ceedca516be03

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+  end
+
+def batch_badge_creator(array)
+    arr = []
+    array.each do |name|
+        arr.push("Hello, my name is #{name}.")
+    end
+    return arr
+end
+
+def assign_rooms(array)
+    arr = []
+    counter = 1
+    array.each do |name|
+        arr.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+        counter += 1
+    end
+    return arr
+end
+
+def printer(array)
+    batch_badge_creator(array).each do |id|
+        puts id
+    end
+    assign_rooms(array).each do |id|
+        puts id
+    end
+end
